009124191f3297c25a608b741b53d4b3465d3533
Christian Haas <christian.haas@sevensuns.at>
2023-09-04 18:38:34 +0200

    Add basic information to about/help dialog

--- frontend/src/cpp/MainWindow.cpp
+++ frontend/src/cpp/MainWindow.cpp
@@ -1,7 +1,9 @@
+#include <string>
 
 #include <raygui/raygui.h>
 #include <raylib.h>
 
+#include "contomap/editor/VersionInfo.h"
 #include "contomap/frontend/MainWindow.h"
 
 using contomap::frontend::MainWindow;
@@ -120,12 +122,35 @@ void MainWindow::drawUserInterface()
    if (viewModelState.helpWindowShown)
    {
       Vector2 windowSize { 320, 200 };
-      auto windowCloseRequested
-         = GuiWindowBox(Rectangle { contentSize.x / 2 - windowSize.x / 2, contentSize.y / 2 - windowSize.y / 2, windowSize.x, windowSize.y }, "#193#Help");
+      Vector2 windowPos { contentSize.x / 2 - windowSize.x / 2, contentSize.y / 2 - windowSize.y / 2 };
+      auto windowCloseRequested = GuiWindowBox(Rectangle { windowPos.x, windowPos.y, windowSize.x, windowSize.y }, "#193#Help");
 
       if (IsKeyPressed(KEY_ESCAPE))
       {
          windowCloseRequested = true;
+      }
+
+      auto propertiesTitleStart = windowPos.x + padding;
+      auto propertiesValueStart = windowPos.x + 50.0f;
+      auto propertiesY = windowPos.y + 40.0f;
+      auto propertiesHeight = 20.0f;
+
+      auto addLine = [windowPos, windowSize, propertiesTitleStart, propertiesValueStart, padding, &propertiesY, propertiesHeight](
+                        std::string const &title, std::string const &value) {
+         GuiLabel(Rectangle { propertiesTitleStart, propertiesY, propertiesValueStart - propertiesTitleStart - padding, propertiesHeight }, title.c_str());
+         GuiLabel(
+            Rectangle { propertiesValueStart, propertiesY, windowPos.x + windowSize.x - propertiesValueStart - padding, propertiesHeight }, value.c_str());
+         propertiesY += propertiesHeight + padding;
+      };
+
+      addLine("Version:", contomap::editor::VersionInfo::GLOBAL.humanReadable());
+
+      auto const codeUrl = "https://github.com/contomap/contomap";
+      GuiLabel(Rectangle { propertiesTitleStart, propertiesY, propertiesValueStart - propertiesTitleStart - padding, propertiesHeight }, "Code:");
+      if (GuiLabelButton(
+             Rectangle { propertiesValueStart, propertiesY, windowPos.x + windowSize.x - propertiesValueStart - padding, propertiesHeight }, codeUrl))
+      {
+         OpenURL(codeUrl);
       }
 
       if (windowCloseRequested)
