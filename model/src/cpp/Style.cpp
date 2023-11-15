35460c5cb9779588df41f2fed71439e46fb809f8
Christian Haas <christian.haas@sevensuns.at>
2023-11-15 19:36:20 +0100

    Refine Style information

--- model/src/cpp/Style.cpp
+++ model/src/cpp/Style.cpp
@@ -3,7 +3,75 @@
 using contomap::model::Identifier;
 using contomap::model::Style;
 
+Style::Color const Style::Set::DEFAULT_TEXT { .red = 0x00, .green = 0x00, .blue = 0x00, .alpha = 0xFF };
+Style::Color const Style::Set::DEFAULT_FILL { .red = 0xFF, .green = 0xFF, .blue = 0xFF, .alpha = 0xFF };
+Style::Color const Style::Set::DEFAULT_LINE { .red = 0x00, .green = 0x00, .blue = 0x00, .alpha = 0xFF };
+
+Style::Set Style::Set::withTextColor(Color value) const
+{
+   Set copy(*this);
+   copy.textColor = value;
+   return copy;
+}
+
+Style::Set Style::Set::withFillColor(Color value) const
+{
+   Set copy(*this);
+   copy.fillColor = value;
+   return copy;
+}
+
+Style::Set Style::Set::withLineColor(Color value) const
+{
+   Set copy(*this);
+   copy.lineColor = value;
+   return copy;
+}
+
+Style::Set Style::Set::withDefaultsFrom(Style const &style) const
+{
+   return withDefaultsFrom(style.toSet());
+}
+
+Style::Set Style::Set::withDefaultsFrom(Set const &other) const
+{
+   Set copy(*this);
+   if (!copy.textColor.has_value())
+   {
+      copy.textColor = other.textColor;
+   }
+   if (!copy.fillColor.has_value())
+   {
+      copy.fillColor = other.fillColor;
+   }
+   if (!copy.lineColor.has_value())
+   {
+      copy.lineColor = other.lineColor;
+   }
+   return copy;
+}
+
+Style::Color Style::Set::getTextColor() const
+{
+   return textColor.value_or(DEFAULT_TEXT);
+}
+
+Style::Color Style::Set::getFillColor() const
+{
+   return fillColor.value_or(DEFAULT_FILL);
+}
+
+Style::Color Style::Set::getLineColor() const
+{
+   return lineColor.value_or(DEFAULT_LINE);
+}
+
 Style::Style(Identifier id)
    : id(id)
 {
+}
+
+Style::Set const &Style::toSet() const
+{
+   return set;
 }
--- model/src/h/contomap/model/Style.h
+++ model/src/h/contomap/model/Style.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include "contomap/model/Identifier.h"
 #include "contomap/model/Identifiers.h"
 #include "contomap/model/OptionalIdentifier.h"
@@ -15,15 +17,114 @@ class Style
 {
 public:
    /**
+    * Color provides the intensities of the red, green, and blue color channels, as well as an alpha value for blending.
+    */
+   struct Color
+   {
+   public:
+      /** Red color channel intensity. */
+      uint8_t red;
+      /** Green color channel intensity. */
+      uint8_t green;
+      /** Blue color channel intensity. */
+      uint8_t blue;
+      /** Alpha blending intensity. */
+      uint8_t alpha;
+   };
+
+   /**
+    * Set represents the sole properties for rendering of a style.
+    */
+   class Set
+   {
+   public:
+      /**
+       * Default constructor.
+       */
+      Set() = default;
+
+      /**
+       * Returns a new set with the copy from this set and the text color overridden.
+       *
+       * @param value the value to override.
+       * @return a new set instance.
+       */
+      [[nodiscard]] Set withTextColor(Color value) const;
+
+      /**
+       * Returns a new set with the copy from this set and the fill color overridden.
+       *
+       * @param value the value to override.
+       * @return a new set instance.
+       */
+      [[nodiscard]] Set withFillColor(Color value) const;
+
+      /**
+       * Returns a new set with the copy from this set and the line color overridden.
+       *
+       * @param value the value to override.
+       * @return a new set instance.
+       */
+      [[nodiscard]] Set withLineColor(Color value) const;
+
+      /**
+       * Returns a new set with a copy from this set and any optionals not yet filled out are defaulting to given style.
+       *
+       * @param style the style from which to take values.
+       * @return a new set instance.
+       */
+      [[nodiscard]] Set withDefaultsFrom(Style const &style) const;
+
+      /**
+       * Returns a new set with a copy from this set and any optionals not yet filled out are defaulting to given set.
+       *
+       * @param other the other set from which to take values.
+       * @return a new set instance.
+       */
+      [[nodiscard]] Set withDefaultsFrom(Set const &other) const;
+
+      /**
+       * @return the text color if complete, black otherwise.
+       */
+      [[nodiscard]] Color getTextColor() const;
+
+      /**
+       * @return the fill color if complete, white otherwise.
+       */
+      [[nodiscard]] Color getFillColor() const;
+
+      /**
+       * @return the line color if complete, black otherwise.
+       */
+      [[nodiscard]] Color getLineColor() const;
+
+   private:
+      static Color const DEFAULT_TEXT;
+      static Color const DEFAULT_FILL;
+      static Color const DEFAULT_LINE;
+
+      std::optional<Color> textColor;
+      std::optional<Color> fillColor;
+      std::optional<Color> lineColor;
+   };
+
+   /**
     * Constructor.
     *
     * @param id the primary identifier of this style.
     */
    explicit Style(contomap::model::Identifier id);
 
+   /**
+    * @return the set of parameters for this style.
+    */
+   [[nodiscard]] Set const &toSet() const;
+
 private:
    contomap::model::Identifier id;
    contomap::model::OptionalIdentifier localTo;
+
+   Set set;
 };
 
 }
