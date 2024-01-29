2c36332bd2dd05528d260cc5868c31e86035a827
Christian Haas <christian.haas@sevensuns.at>
2024-01-29 18:18:16 +0100

    Extract Traits header

--- editor/src/h/contomap/editor/Selections.h
+++ editor/src/h/contomap/editor/Selections.h
@@ -1,47 +1,14 @@
 #pragma once
 
-#include <type_traits>
-
 #include "contomap/editor/Selection.h"
 #include "contomap/infrastructure/Generator.h"
+#include "contomap/infrastructure/Traits.h"
 #include "contomap/model/Contomap.h"
 #include "contomap/model/ContomapView.h"
 #include "contomap/model/Style.h"
 
 namespace contomap::editor
 {
-
-/**
- * constness_of allows to forward the const state of a source type to another one.
- * From https://codereview.stackexchange.com/questions/208121/add-const-or-remove-const-based-on-another-type
- *
- * @tparam U the source type to consider
- */
-template <typename U> struct constness_of
-{
-   enum
-   {
-      value = std::is_const<U>::value
-   };
-
-   /**
-    * applied_to sets the const state according to the source type
-    *
-    * @tparam T the type to apply to
-    */
-   template <typename T> struct applied_to
-   {
-      /**
-       * The resulting type.
-       */
-      using type = typename std::conditional<value, typename std::add_const<T>::type, typename std::remove_const<T>::type>::type;
-   };
-
-   /**
-    * Forwarder type.
-    */
-   template <typename T> using applied_to_t = typename applied_to<T>::type;
-};
 
 /**
  * Selections provides helper functions for resolving items.
@@ -56,7 +23,8 @@ public:
     * @return the reference to the found occurrence - if selected and existing.
     */
    template <class SelectionType>
-   [[nodiscard]] static std::optional<std::reference_wrapper<typename constness_of<SelectionType>::template applied_to_t<contomap::model::Occurrence>>>
+   [[nodiscard]] static std::optional<
+      std::reference_wrapper<typename contomap::infrastructure::constness_of<SelectionType>::template applied_to_t<contomap::model::Occurrence>>>
    firstOccurrenceFrom(SelectionType &selection)
    {
       for (auto &occurrence : selection.template of<contomap::model::Occurrence>())
