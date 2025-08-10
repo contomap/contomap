299ef52e5325b35e5770de9f44ed89ba164985be
Christian Haas <christian.haas@sevensuns.at>
2025-08-10 21:35:01 +0200

    Update CMakeRC to newest version

--- CMakeRC.cmake
+++ CMakeRC.cmake
@@ -1,6 +1,6 @@
 # This block is executed when generating an intermediate resource file, not when
 # running in CMake configure mode
-if(_CMRC_GENERATE_MODE)
+if (_CMRC_GENERATE_MODE)
     # Read in the digits
     file(READ "${INPUT_FILE}" bytes HEX)
     # Format each pair into a character literal. Heuristics seem to favor doing
@@ -11,17 +11,17 @@ if(_CMRC_GENERATE_MODE)
     math(EXPR n_bytes "${n_bytes2} / 2")
     math(EXPR remainder "${n_bytes} % 5") # <-- '5' is the grouping count from above
     set(cleanup_re "$")
-    set(cleanup_sub )
-    while(remainder)
+    set(cleanup_sub)
+    while (remainder)
         set(cleanup_re "(..)${cleanup_re}")
         set(cleanup_sub "'\\\\x\\${remainder}',${cleanup_sub}")
         math(EXPR remainder "${remainder} - 1")
-    endwhile()
-    if(NOT cleanup_re STREQUAL "$")
+    endwhile ()
+    if (NOT cleanup_re STREQUAL "$")
         string(REGEX REPLACE "${cleanup_re}" "${cleanup_sub}" chars "${chars}")
-    endif()
+    endif ()
     string(CONFIGURE [[
-        namespace { const char file_array[] = { @chars@ }; }
+        namespace { const char file_array[] = { @chars@ 0 }; }
         namespace cmrc { namespace @NAMESPACE@ { namespace res_chars {
         extern const char* const @SYMBOL@_begin = file_array;
         extern const char* const @SYMBOL@_end = file_array + @n_bytes@;
@@ -30,20 +30,20 @@ if(_CMRC_GENERATE_MODE)
     file(WRITE "${OUTPUT_FILE}" "${code}")
     # Exit from the script. Nothing else needs to be processed
     return()
-endif()
+endif ()
 
 set(_version 2.0.0)
 
-cmake_minimum_required(VERSION 3.3)
+cmake_minimum_required(VERSION 3.12)
 include(CMakeParseArguments)
 
-if(COMMAND cmrc_add_resource_library)
-    if(NOT DEFINED _CMRC_VERSION OR NOT (_version STREQUAL _CMRC_VERSION))
+if (COMMAND cmrc_add_resource_library)
+    if (NOT DEFINED _CMRC_VERSION OR NOT (_version STREQUAL _CMRC_VERSION))
         message(WARNING "More than one CMakeRC version has been included in this project.")
-    endif()
+    endif ()
     # CMakeRC has already been included! Don't do anything
     return()
-endif()
+endif ()
 
 set(_CMRC_VERSION "${_version}" CACHE INTERNAL "CMakeRC version. Used for checking for conflicts")
 
@@ -52,9 +52,9 @@ set(_CMRC_SCRIPT "${CMAKE_CURRENT_LIST_FILE}" CACHE INTERNAL "Path to CMakeRC sc
 function(_cmrc_normalize_path var)
     set(path "${${var}}")
     file(TO_CMAKE_PATH "${path}" path)
-    while(path MATCHES "//")
+    while (path MATCHES "//")
         string(REPLACE "//" "/" path "${path}")
-    endwhile()
+    endwhile ()
     string(REGEX REPLACE "/+$" "" path "${path}")
     set("${var}" "${path}" PARENT_SCOPE)
 endfunction()
@@ -76,6 +76,10 @@ set(hpp_content [==[
 #include <string>
 #include <system_error>
 #include <type_traits>
+
+#if !(defined(__EXCEPTIONS) || defined(__cpp_exceptions) || defined(_CPPUNWIND) || defined(CMRC_NO_EXCEPTIONS))
+#define CMRC_NO_EXCEPTIONS 1
+#endif
 
 namespace cmrc { namespace detail { struct dummy; } }
 
@@ -101,7 +105,7 @@ public:
     iterator cbegin() const noexcept { return _begin; }
     iterator end() const noexcept { return _end; }
     iterator cend() const noexcept { return _end; }
-    std::size_t size() const { return std::distance(begin(), end()); }
+    std::size_t size() const { return static_cast<std::size_t>(std::distance(begin(), end())); }
 
     file() = default;
     file(iterator beg, iterator end) noexcept : _begin(beg), _end(end) {}
@@ -182,14 +186,14 @@ public:
     created_subdirectory add_subdir(std::string name) & {
         _dirs.emplace_back();
         auto& back = _dirs.back();
-        auto& fod = _index.emplace(name, back).first->second;
+        auto& fod = _index.emplace(name, file_or_directory{back}).first->second;
         return created_subdirectory{back, fod};
     }
 
     file_or_directory* add_file(std::string name, const char* begin, const char* end) & {
         assert(_index.find(name) == _index.end());
         _files.emplace_back(begin, end);
-        return &_index.emplace(name, _files.back()).first->second;
+        return &_index.emplace(name, file_or_directory{_files.back()}).first->second;
     }
 
     const file_or_directory* get(const std::string& path) const {
@@ -243,15 +247,15 @@ public:
             return !(*this == rhs);
         }
 
-        iterator operator++() noexcept {
+        iterator& operator++() noexcept {
+            ++_base_iter;
+            return *this;
+        }
+
+        iterator operator++(int) noexcept {
             auto cp = *this;
             ++_base_iter;
             return cp;
-        }
-
-        iterator& operator++(int) noexcept {
-            ++_base_iter;
-            return *this;
         }
     };
 
@@ -275,7 +279,7 @@ inline std::string normalize_path(std::string path) {
     }
     auto off = path.npos;
     while ((off = path.find("//")) != path.npos) {
-        path.erase(path.begin() + off);
+        path.erase(path.begin() + static_cast<std::string::difference_type>(off));
     }
     return path;
 }
@@ -339,7 +343,12 @@ public:
     file open(const std::string& path) const {
         auto entry_ptr = _get(path);
         if (!entry_ptr || !entry_ptr->is_file()) {
+#ifdef CMRC_NO_EXCEPTIONS
+            fprintf(stderr, "Error no such file or directory: %s\n", path.c_str());
+            abort();
+#else
             throw std::system_error(make_error_code(std::errc::no_such_file_or_directory), path);
+#endif
         }
         auto& dat = entry_ptr->as_file();
         return file{dat.begin_ptr, dat.end_ptr};
@@ -362,10 +371,20 @@ public:
     directory_iterator iterate_directory(const std::string& path) const {
         auto entry_ptr = _get(path);
         if (!entry_ptr) {
+#ifdef CMRC_NO_EXCEPTIONS
+            fprintf(stderr, "Error no such file or directory: %s\n", path.c_str());
+            abort();
+#else
             throw std::system_error(make_error_code(std::errc::no_such_file_or_directory), path);
+#endif
         }
         if (!entry_ptr->is_directory()) {
+#ifdef CMRC_NO_EXCEPTIONS
+            fprintf(stderr, "Error not a directory: %s\n", path.c_str());
+            abort();
+#else
             throw std::system_error(make_error_code(std::errc::not_a_directory), path);
+#endif
         }
         return entry_ptr->as_directory().begin();
     }
@@ -378,38 +397,46 @@ public:
 
 set(cmrc_hpp "${CMRC_INCLUDE_DIR}/cmrc/cmrc.hpp" CACHE INTERNAL "")
 set(_generate 1)
-if(EXISTS "${cmrc_hpp}")
+if (EXISTS "${cmrc_hpp}")
     file(READ "${cmrc_hpp}" _current)
-    if(_current STREQUAL hpp_content)
+    if (_current STREQUAL hpp_content)
         set(_generate 0)
-    endif()
-endif()
+    endif ()
+endif ()
 file(GENERATE OUTPUT "${cmrc_hpp}" CONTENT "${hpp_content}" CONDITION ${_generate})
 
 add_library(cmrc-base INTERFACE)
-target_include_directories(cmrc-base INTERFACE "${CMRC_INCLUDE_DIR}")
+target_include_directories(cmrc-base INTERFACE $<BUILD_INTERFACE:${CMRC_INCLUDE_DIR}>)
 # Signal a basic C++11 feature to require C++11.
 target_compile_features(cmrc-base INTERFACE cxx_nullptr)
 set_property(TARGET cmrc-base PROPERTY INTERFACE_CXX_EXTENSIONS OFF)
 add_library(cmrc::base ALIAS cmrc-base)
 
 function(cmrc_add_resource_library name)
-    set(args ALIAS NAMESPACE)
+    set(args ALIAS NAMESPACE TYPE)
     cmake_parse_arguments(ARG "" "${args}" "" "${ARGN}")
     # Generate the identifier for the resource library's namespace
     set(ns_re "[a-zA-Z_][a-zA-Z0-9_]*")
-    if(NOT DEFINED ARG_NAMESPACE)
+    if (NOT DEFINED ARG_NAMESPACE)
         # Check that the library name is also a valid namespace
-        if(NOT name MATCHES "${ns_re}")
+        if (NOT name MATCHES "${ns_re}")
             message(SEND_ERROR "Library name is not a valid namespace. Specify the NAMESPACE argument")
-        endif()
+        endif ()
         set(ARG_NAMESPACE "${name}")
-    else()
-        if(NOT ARG_NAMESPACE MATCHES "${ns_re}")
+    else ()
+        if (NOT ARG_NAMESPACE MATCHES "${ns_re}")
             message(SEND_ERROR "NAMESPACE for ${name} is not a valid C++ namespace identifier (${ARG_NAMESPACE})")
-        endif()
-    endif()
+        endif ()
+    endif ()
     set(libname "${name}")
+    # Check that type is either "STATIC" or "OBJECT", or default to "STATIC" if
+    # not set
+    if (NOT DEFINED ARG_TYPE)
+        set(ARG_TYPE STATIC)
+    elseif (NOT "${ARG_TYPE}" MATCHES "^(STATIC|OBJECT)$")
+        message(SEND_ERROR "${ARG_TYPE} is not a valid TYPE (STATIC and OBJECT are acceptable)")
+        set(ARG_TYPE STATIC)
+    endif ()
     # Generate a library with the compiled in character arrays.
     string(CONFIGURE [=[
         #include <cmrc/cmrc.hpp>
@@ -461,70 +488,70 @@ function(cmrc_add_resource_library name)
     file(GENERATE OUTPUT "${lib_tmp_cpp}" CONTENT "${cpp_content}")
     get_filename_component(libcpp "${libdir}/lib.cpp" ABSOLUTE)
     add_custom_command(OUTPUT "${libcpp}"
-        DEPENDS "${lib_tmp_cpp}" "${cmrc_hpp}"
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${lib_tmp_cpp}" "${libcpp}"
-        COMMENT "Generating ${name} resource loader"
-        )
+            DEPENDS "${lib_tmp_cpp}" "${cmrc_hpp}"
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different "${lib_tmp_cpp}" "${libcpp}"
+            COMMENT "Generating ${name} resource loader"
+    )
     # Generate the actual static library. Each source file is just a single file
     # with a character array compiled in containing the contents of the
     # corresponding resource file.
-    add_library(${name} STATIC ${libcpp})
+    add_library(${name} ${ARG_TYPE} ${libcpp})
     set_property(TARGET ${name} PROPERTY CMRC_LIBDIR "${libdir}")
     set_property(TARGET ${name} PROPERTY CMRC_NAMESPACE "${ARG_NAMESPACE}")
     target_link_libraries(${name} PUBLIC cmrc::base)
     set_property(TARGET ${name} PROPERTY CMRC_IS_RESOURCE_LIBRARY TRUE)
-    if(ARG_ALIAS)
+    if (ARG_ALIAS)
         add_library("${ARG_ALIAS}" ALIAS ${name})
-    endif()
+    endif ()
     cmrc_add_resources(${name} ${ARG_UNPARSED_ARGUMENTS})
 endfunction()
 
 function(_cmrc_register_dirs name dirpath)
-    if(dirpath STREQUAL "")
+    if (dirpath STREQUAL "")
         return()
-    endif()
+    endif ()
     # Skip this dir if we have already registered it
     get_target_property(registered "${name}" _CMRC_REGISTERED_DIRS)
-    if(dirpath IN_LIST registered)
+    if (dirpath IN_LIST registered)
         return()
-    endif()
+    endif ()
     # Register the parent directory first
     get_filename_component(parent "${dirpath}" DIRECTORY)
-    if(NOT parent STREQUAL "")
+    if (NOT parent STREQUAL "")
         _cmrc_register_dirs("${name}" "${parent}")
-    endif()
+    endif ()
     # Now generate the registration
     set_property(TARGET "${name}" APPEND PROPERTY _CMRC_REGISTERED_DIRS "${dirpath}")
     _cm_encode_fpath(sym "${dirpath}")
-    if(parent STREQUAL "")
+    if (parent STREQUAL "")
         set(parent_sym root_directory)
-    else()
+    else ()
         _cm_encode_fpath(parent_sym "${parent}")
-    endif()
+    endif ()
     get_filename_component(leaf "${dirpath}" NAME)
     set_property(
-        TARGET "${name}"
-        APPEND PROPERTY CMRC_MAKE_DIRS
-        "static auto ${sym}_dir = ${parent_sym}_dir.directory.add_subdir(\"${leaf}\")\;"
-        "root_index.emplace(\"${dirpath}\", &${sym}_dir.index_entry)\;"
-        )
+            TARGET "${name}"
+            APPEND PROPERTY CMRC_MAKE_DIRS
+            "static auto ${sym}_dir = ${parent_sym}_dir.directory.add_subdir(\"${leaf}\")\;"
+            "root_index.emplace(\"${dirpath}\", &${sym}_dir.index_entry)\;"
+    )
 endfunction()
 
 function(cmrc_add_resources name)
     get_target_property(is_reslib ${name} CMRC_IS_RESOURCE_LIBRARY)
-    if(NOT TARGET ${name} OR NOT is_reslib)
+    if (NOT TARGET ${name} OR NOT is_reslib)
         message(SEND_ERROR "cmrc_add_resources called on target '${name}' which is not an existing resource library")
         return()
-    endif()
+    endif ()
 
     set(options)
     set(args WHENCE PREFIX)
     set(list_args)
     cmake_parse_arguments(ARG "${options}" "${args}" "${list_args}" "${ARGN}")
 
-    if(NOT ARG_WHENCE)
+    if (NOT ARG_WHENCE)
         set(ARG_WHENCE ${CMAKE_CURRENT_SOURCE_DIR})
-    endif()
+    endif ()
     _cmrc_normalize_path(ARG_WHENCE)
     get_filename_component(ARG_WHENCE "${ARG_WHENCE}" ABSOLUTE)
 
@@ -534,78 +561,78 @@ function(cmrc_add_resources name)
     get_target_property(libdir ${name} CMRC_LIBDIR)
     get_target_property(target_dir ${name} SOURCE_DIR)
     file(RELATIVE_PATH reldir "${target_dir}" "${CMAKE_CURRENT_SOURCE_DIR}")
-    if(reldir MATCHES "^\\.\\.")
+    if (reldir MATCHES "^\\.\\.")
         message(SEND_ERROR "Cannot call cmrc_add_resources in a parent directory from the resource library target")
         return()
-    endif()
+    endif ()
 
-    foreach(input IN LISTS ARG_UNPARSED_ARGUMENTS)
+    foreach (input IN LISTS ARG_UNPARSED_ARGUMENTS)
         _cmrc_normalize_path(input)
         get_filename_component(abs_in "${input}" ABSOLUTE)
         # Generate a filename based on the input filename that we can put in
         # the intermediate directory.
         file(RELATIVE_PATH relpath "${ARG_WHENCE}" "${abs_in}")
-        if(relpath MATCHES "^\\.\\.")
+        if (relpath MATCHES "^\\.\\.")
             # For now we just error on files that exist outside of the soure dir.
             message(SEND_ERROR "Cannot add file '${input}': File must be in a subdirectory of ${ARG_WHENCE}")
             continue()
-        endif()
-        if(DEFINED ARG_PREFIX)
+        endif ()
+        if (DEFINED ARG_PREFIX)
             _cmrc_normalize_path(ARG_PREFIX)
-        endif()
-        if(ARG_PREFIX AND NOT ARG_PREFIX MATCHES "/$")
+        endif ()
+        if (ARG_PREFIX AND NOT ARG_PREFIX MATCHES "/$")
             set(ARG_PREFIX "${ARG_PREFIX}/")
-        endif()
+        endif ()
         get_filename_component(dirpath "${ARG_PREFIX}${relpath}" DIRECTORY)
         _cmrc_register_dirs("${name}" "${dirpath}")
-        get_filename_component(abs_out "${libdir}/intermediate/${relpath}.cpp" ABSOLUTE)
+        get_filename_component(abs_out "${libdir}/intermediate/${ARG_PREFIX}${relpath}.cpp" ABSOLUTE)
         # Generate a symbol name relpath the file's character array
         _cm_encode_fpath(sym "${relpath}")
         # Get the symbol name for the parent directory
-        if(dirpath STREQUAL "")
+        if (dirpath STREQUAL "")
             set(parent_sym root_directory)
-        else()
+        else ()
             _cm_encode_fpath(parent_sym "${dirpath}")
-        endif()
+        endif ()
         # Generate the rule for the intermediate source file
         _cmrc_generate_intermediate_cpp(${lib_ns} ${sym} "${abs_out}" "${abs_in}")
         target_sources(${name} PRIVATE "${abs_out}")
         set_property(TARGET ${name} APPEND PROPERTY CMRC_EXTERN_DECLS
-            "// Pointers to ${input}"
-            "extern const char* const ${sym}_begin\;"
-            "extern const char* const ${sym}_end\;"
-            )
+                "// Pointers to ${input}"
+                "extern const char* const ${sym}_begin\;"
+                "extern const char* const ${sym}_end\;"
+        )
         get_filename_component(leaf "${relpath}" NAME)
         set_property(
-            TARGET ${name}
-            APPEND PROPERTY CMRC_MAKE_FILES
-            "root_index.emplace("
-            "    \"${ARG_PREFIX}${relpath}\","
-            "    ${parent_sym}_dir.directory.add_file("
-            "        \"${leaf}\","
-            "        res_chars::${sym}_begin,"
-            "        res_chars::${sym}_end"
-            "    )"
-            ")\;"
-            )
-    endforeach()
+                TARGET ${name}
+                APPEND PROPERTY CMRC_MAKE_FILES
+                "root_index.emplace("
+                "    \"${ARG_PREFIX}${relpath}\","
+                "    ${parent_sym}_dir.directory.add_file("
+                "        \"${leaf}\","
+                "        res_chars::${sym}_begin,"
+                "        res_chars::${sym}_end"
+                "    )"
+                ")\;"
+        )
+    endforeach ()
 endfunction()
 
 function(_cmrc_generate_intermediate_cpp lib_ns symbol outfile infile)
     add_custom_command(
-        # This is the file we will generate
-        OUTPUT "${outfile}"
-        # These are the primary files that affect the output
-        DEPENDS "${infile}" "${_CMRC_SCRIPT}"
-        COMMAND
+            # This is the file we will generate
+            OUTPUT "${outfile}"
+            # These are the primary files that affect the output
+            DEPENDS "${infile}" "${_CMRC_SCRIPT}"
+            COMMAND
             "${CMAKE_COMMAND}"
-                -D_CMRC_GENERATE_MODE=TRUE
-                -DNAMESPACE=${lib_ns}
-                -DSYMBOL=${symbol}
-                "-DINPUT_FILE=${infile}"
-                "-DOUTPUT_FILE=${outfile}"
-                -P "${_CMRC_SCRIPT}"
-        COMMENT "Generating intermediate file for ${infile}"
+            -D_CMRC_GENERATE_MODE=TRUE
+            -DNAMESPACE=${lib_ns}
+            -DSYMBOL=${symbol}
+            "-DINPUT_FILE=${infile}"
+            "-DOUTPUT_FILE=${outfile}"
+            -P "${_CMRC_SCRIPT}"
+            COMMENT "Generating intermediate file for ${infile}"
     )
 endfunction()
 
